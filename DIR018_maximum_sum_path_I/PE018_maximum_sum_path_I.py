c90287f8d533813e7131c58d30cdaf8335321434
John Baxter <john.g.baxter@googlemail.com>
2020-12-03 16:20:18 +0000

    Add extra code and memoisation_cache dict to maximum_sum_path_I; memoisation successfully implemented.

--- DIR018_maximum_sum_path_I/PE018_maximum_sum_path_I.py
+++ DIR018_maximum_sum_path_I/PE018_maximum_sum_path_I.py
@@ -1,26 +1,20 @@
-example_triangle = [
-  [3],
-  [7,4],
-  [2,4,6],
-  [8,5,9,3]]
+problem_triangle_string_I = """75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
 
-problem_triangle_string = """75
-  95 64
-  17 47 82
-  18 35 87 10
-  20 04 82 47 65
-  19 01 23 75 03 34
-  88 02 77 73 07 63 67
-  99 65 04 28 06 16 70 92
-  41 41 26 56 83 40 80 70 33
-  41 48 72 33 47 32 37 16 94 29
-  53 71 44 65 25 43 91 52 97 51 14
-  70 11 33 28 77 73 17 78 39 68 17 57
-  91 71 52 38 17 14 91 43 58 50 27 29 48
-  63 66 04 68 89 53 67 30 73 16 69 87 40 31
-  04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"""
-
-problem_triangle_list_of_strings = problem_triangle_string.split('\n')
+problem_triangle_list_of_strings = problem_triangle_string_I.split('\n')
 problem_triangle_list_of_lists = [line.split(' ') for line in problem_triangle_list_of_strings]
 problem_triangle_as_ints = \
 [[int(strint) for strint in line] for line in problem_triangle_list_of_lists]
@@ -41,11 +35,14 @@ def early_return_check(triangle):
   if len(triangle) < 2:
     return True
 
+def memoisation_check(triangle):
+  if str(triangle) in memoisation_cache:
+    return True
+
 def split_triangle(triangle):
   top = triangle[0][0]
   left_part = remove_top_and_right_edge(triangle)
   right_part = remove_top_and_left_edge(triangle)
-
   return (top, left_part, right_part)
 
 def calculate_path_total(single_int, left_sum, right_sum):
@@ -56,6 +53,9 @@ def recursive_sum_path_calculation(triangle):
   if early_return_check(triangle):
     return triangle[0][0]
 
+  if memoisation_check(triangle):
+    return memoisation_cache[str(triangle)]
+
   top = split_triangle(triangle)[0]
   left_part = split_triangle(triangle)[1]
   right_part = split_triangle(triangle)[2]
@@ -65,7 +65,10 @@ def recursive_sum_path_calculation(triangle):
 
   if type(left_part) is int and type(right_part) is int:
     path_sum = calculate_path_total(top, left_part, right_part)
+    memoisation_cache[str(triangle)] = path_sum
     return path_sum
+
+memoisation_cache = {}
 
 if __name__ == '__main__':
   print(recursive_sum_path_calculation(problem_triangle_as_ints))
