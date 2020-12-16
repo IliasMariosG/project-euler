25d195f70541fdd65c9a5f350517dc703bd1dd1b
John Baxter <john.g.baxter@googlemail.com>
2020-12-16 13:32:47 +0000

    WIP: getting python code ready for thirs step of solution.

--- DIR023_non_abundant_sums/PE023_non_abundant_sums.py
+++ DIR023_non_abundant_sums/PE023_non_abundant_sums.py
@@ -13,6 +13,14 @@ def find_all_abundant_numbers_below(limit):
       list_of_abundants.append(i)
   return list_of_abundants
 
+def check_if_number_can_be_expressed_as_sum_of_two_list_elements(number, list):
+  for i in list:
+    if (number - i) in list:
+      return True
+  return False
+
+
+
 
 if __name__ == '__main__':
   # print(is_it_abundant_or_not(28))
