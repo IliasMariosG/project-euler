96b52183519d70fcd47a3449847d439ecbef28ca
John Baxter <john.g.baxter@googlemail.com>
2020-12-16 13:08:22 +0000

    Add python code for first step of solution. Can show if a number is abundant or not.

--- DIR023_non_abundant_sums/PE023_non_abundant_sums.py
+++ DIR023_non_abundant_sums/PE023_non_abundant_sums.py
@@ -1,1 +1,11 @@
 from euler.functions import get_list_of_factors
+
+def is_it_abundant_or_not(number):
+  if sum(get_list_of_factors(number)) > number:
+    return True
+  else:
+    return False
+
+
+if __name__ == '__main__':
+  # print(is_it_abundant_or_not(28))
