2c1f666bcc3e87da53958847d99a6580058813a8
John Baxter <john.g.baxter@googlemail.com>
2020-12-16 13:16:58 +0000

    Add python code for secont step of solution. Can prepare list of abundant numbers below a given limit.

--- DIR023_non_abundant_sums/PE023_non_abundant_sums.py
+++ DIR023_non_abundant_sums/PE023_non_abundant_sums.py
@@ -6,6 +6,14 @@ def is_it_abundant_or_not(number):
   else:
     return False
 
+def find_all_abundant_numbers_below(limit):
+  list_of_abundants = []
+  for i in range(limit):
+    if is_it_abundant_or_not(i):
+      list_of_abundants.append(i)
+  return list_of_abundants
+
 
 if __name__ == '__main__':
   # print(is_it_abundant_or_not(28))
+  print(find_all_abundant_numbers_below(100))
